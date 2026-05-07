d9a7ec7d3d3227e792cf16cb9c0d224e5455a8f7
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:25:42 -0600

    Remove unused fixtures from e2e tests
    Kept logScalePage and workflowsPage which are used in tests. Removed FoundryHomePage, AppCatalogPage, appName, and config fixtures that were defined but never destructured.

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,39 +1,19 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  FoundryHomePage,
-  AppCatalogPage,
-  WorkflowsPage,
-  config,
-} from '@crowdstrike/foundry-playwright';
+import { WorkflowsPage } from '@crowdstrike/foundry-playwright';
 import { LogScalePage } from './pages/LogScalePage';
 
 type FoundryFixtures = {
-  foundryHomePage: FoundryHomePage;
-  appCatalogPage: AppCatalogPage;
   logScalePage: LogScalePage;
   workflowsPage: WorkflowsPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  foundryHomePage: async ({ page }, use) => {
-    await use(new FoundryHomePage(page));
-  },
-
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
   logScalePage: async ({ page }, use) => {
     await use(new LogScalePage(page));
   },
 
   workflowsPage: async ({ page }, use) => {
     await use(new WorkflowsPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
